5d6c2e659fbee33e1ae371bbf5adda4a5a773a41
MinMinLight <cascio.email@gmail.com>
2017-06-14 22:05:50 +0200

    changement des path dans le head (css) et le JS (javascript)

--- structure/JS.php
+++ structure/JS.php
@@ -1,9 +1,9 @@
 <!-- jQwery -->
-<script src="<?php echo $path_root ; ?>jquery-3.2.1.min.js"></script>
+<script src="<?php echo $path_root ; ?>ressources/jquery-3.2.1.min.js"></script>
 <!-- Ajax -->
-<script src="<?php echo $path_root ; ?>tether.min.js"></script>
+<script src="<?php echo $path_root ; ?>ressources/tether.min.js"></script>
 <!-- Bootstrap -->
-<script src="<?php echo $path_root ; ?>bootstrap.min.js"></script>
+<script src="<?php echo $path_root ; ?>ressources/bootstrap.min.js"></script>
 <!-- Google Maps-->
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDqgqboFmTx_a7rxzlrFPjZhRbDMzpUiy8"></script>
 <!-- Fonctions Javascript -->
--- structure/head.php
+++ structure/head.php
@@ -3,11 +3,11 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<!-- Bootstrap -->
-	<link rel="stylesheet" href="<?php echo $path_root ; ?>bootstrap.min.css">
+	<link rel="stylesheet" href="<?php echo $path_root ; ?>ressources/bootstrap.min.css">
 	<!-- Google Material Icons-->
-	<link rel="stylesheet" href="<?php echo $path_root ; ?>material-design-icons-3.0.1/iconfont/material-icons.css">
+	<link rel="stylesheet" href="<?php echo $path_root ; ?>ressources/material-design-icons-3.0.1/iconfont/material-icons.css">
 	<!-- Font Awesome -->
-	<link rel="stylesheet" href="<?php echo $path_root ; ?>font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="<?php echo $path_root ; ?>ressources/font-awesome-4.7.0/css/font-awesome.min.css">
 	<!-- Custom CSS -->
 	<link rel="stylesheet" href="<?php echo $path_root ; ?>style.css" type="text/css" media="screen">
 </head>
