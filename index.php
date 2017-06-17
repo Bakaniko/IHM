b115835ce1a7b0fd2c40fda2b606400a55958fa8
MinMinLight <cascio.email@gmail.com>
2017-06-17 16:18:30 +0200

    update mise en page
    alignement input / labels des formulaires
    modif du pied en sticky (colle au bas de page mais ne recouvre pas le container principal lors de la redimention verticale de la page.
    meilleure exploitation horizontale de l'espace de la page.

--- index.php
+++ index.php
@@ -14,24 +14,29 @@ $path_images=$path_root."images/";
 	<!-- Contenu de la page d'accueil -->
 	
 	<div class="container" id="main">
-		<div class="card mx-auto">
-			<div class="card-block text-center">
-				<h4 class="card-title"><a href="<?php echo $path_pages ; ?>spectacle.php" class="card-link">Date et Titre du spectacle à venir</a></h4>
-				<p class="card-text">Présentation du spectacle</p>
+		<div class="card-deck">
+			<div class="card mx-auto">
+				<div class="card-block">
+					<h4 class="card-title"><a href="<?php echo $path_pages ; ?>spectacle.php" class="card-link">Date et Titre du spectacle à venir</a></h4>
+					<p class="card-text">Présentation du spectacle</p>
+				</div>
+				<ul class="list-group list-group-flush">
+					<li class="list-group-item"><a href="<?php echo $path_pages ; ?>spectacle.php" class="card-link">Date et titre du spectacle + informations</a></li>
+					<li class="list-group-item"><a href="<?php echo $path_pages ; ?>spectacle.php" class="card-link">Date et titre du spectacle + informations</a></li>
+					<li class="list-group-item"><a href="<?php echo $path_pages ; ?>spectacle.php" class="card-link">Date et titre du spectacle + informations</a></li>
+					<li class="list-group-item"><a href="<?php echo $path_pages ; ?>spectacle.php" class="card-link">Date et titre du spectacle + informations</a></li>
+					<li class="list-group-item"><a href="<?php echo $path_pages ; ?>spectacle.php" class="card-link">Date et titre du spectacle + informations</a></li>
+					<li class="list-group-item"><a href="<?php echo $path_pages ; ?>spectacle.php" class="card-link">Date et titre du spectacle + informations</a></li>
+					<li class="list-group-item"><a href="<?php echo $path_pages ; ?>spectacle.php" class="card-link">Date et titre du spectacle + informations</a></li>
+				</ul>	
 			</div>
-			<img class="card-img-bottom img-fluid d-block mx-auto" src="<?php echo $path_images ; ?>les_contes_d_hoffmann_1_christian_leiber_opera_national_de_paris.jpg" alt="Titre du spectacle">
-			<ul class="list-group list-group-flush mt-3">
-				<li class="list-group-item"><a href="<?php echo $path_pages ; ?>spectacle.php" class="card-link">Date et titre du spectacle + informations</a></li>
-				<li class="list-group-item"><a href="<?php echo $path_pages ; ?>spectacle.php" class="card-link">Date et titre du spectacle + informations</a></li>
-				<li class="list-group-item"><a href="<?php echo $path_pages ; ?>spectacle.php" class="card-link">Date et titre du spectacle + informations</a></li>
-				<li class="list-group-item"><a href="<?php echo $path_pages ; ?>spectacle.php" class="card-link">Date et titre du spectacle + informations</a></li>
-				<li class="list-group-item"><a href="<?php echo $path_pages ; ?>spectacle.php" class="card-link">Date et titre du spectacle + informations</a></li>
-				<li class="list-group-item"><a href="<?php echo $path_pages ; ?>spectacle.php" class="card-link">Date et titre du spectacle + informations</a></li>
-				<li class="list-group-item"><a href="<?php echo $path_pages ; ?>spectacle.php" class="card-link">Date et titre du spectacle + informations</a></li>
-			</ul>		
+			<div class="card m-auto">
+				<img class="card-img-bottom img-fluid d-block mx-auto" src="<?php echo $path_images ; ?>les_contes_d_hoffmann_1_christian_leiber_opera_national_de_paris.jpg" alt="Titre du spectacle">
+			</div>
 		</div>
 	</div>
-	<?php include($path_structure."footer.php"); ?> <!-- Inclusion pied de page -->
-	<?php include($path_structure."JS.php"); ?>	<!-- Inclusion CDN et fichiers javascript -->
+</div>
+<?php include($path_structure."footer.php"); ?> <!-- Inclusion pied de page -->
+<?php include($path_structure."JS.php"); ?>	<!-- Inclusion CDN et fichiers javascript -->
 </body>
 </html>
--- pages/infos.php
+++ pages/infos.php
@@ -6,68 +6,78 @@ $path_images=$path_root."images/";
 ?>
 
 <!DOCTYPE html>
-<html lang="fr" class="w-100 h-100">
+<html lang="fr" class="">
 <?php include($path_structure."head.php"); ?>	<!-- Inclusion <head> -->
-<body class="w-100 h-100">
+<body class="">
 	<?php include($path_structure."menu.php"); ?>	<!-- Inclusion menu -->
 
 	<!-- Contenu de la page infos -->
 
 	<div class="container" id="main">
-		<div class="card mx-auto">
-			<div class="card-block">
-				<h4 class="card-title text-center">Nous contacter</h4>
-				<p class="card-text text-justify">
-					Par téléphone au 08 92 89 90 90 (0,35 € TTC/min depuis un poste fixe hors coût éventuel selon opérateur) 
-					ou au + 33 1 71 25 24 23 depuis l’étranger, du lundi au samedi de 9h à 19h.
-				</p>
-				<p class="card-text text-justify">
-					Aux guichets du Palais Garnier (l’angle des rues Scribe et Auber - 75009 Paris) 
-					et de l’Opéra Bastille (130 rue de Lyon - 75012 Paris) du lundi au samedi de 11h30 à 18h30 (sauf jours fériés) 
-					et une heure avant le début des représentations.
-				</p>
+			<div class="card mx-auto">
+				<div class="card-block">
+					<h4 class="card-title text-center">Nous contacter</h4>
+					<p class="card-text text-justify">
+						Par téléphone au 08 92 89 90 90 (0,35 € TTC/min depuis un poste fixe hors coût éventuel selon opérateur) 
+						ou au + 33 1 71 25 24 23 depuis l’étranger, du lundi au samedi de 9h à 19h.
+					</p>
+					<p class="card-text text-justify">
+						Aux guichets du Palais Garnier (l’angle des rues Scribe et Auber - 75009 Paris) 
+						et de l’Opéra Bastille (130 rue de Lyon - 75012 Paris) du lundi au samedi de 11h30 à 18h30 (sauf jours fériés) 
+						et une heure avant le début des représentations.
+					</p>
 
+				</div>
 			</div>
-			<!-- Formulaire -->
-			<div class="card-block">
-				<h5 class="card-title text-center">Par email</h5>
-				<form class="d-flex flex-column justify-content-center" action="" method="post">
-					<div class="form-group d-flex flex-row flex-wrap justify-content-center">
-						<label class="col-2 my-auto" for="prenom">Prénom</label>
-						<input type="text" id="prenom"  class="form-control" name="user_firstname" />
-					</div>		
-					<div class="form-group d-flex flex-row flex-wrap justify-content-center">
-						<label class="col-2 my-auto" for="nom">Nom</label>
-						<input type="text" id="nom" class="form-control" name="user_name"  />
-					</div>									
-					<div class="form-group d-flex flex-row flex-wrap justify-content-center">
-						<label class="col-2 my-auto" for="couriel">Email</label>
-						<input type="email" id="courriel" class="form-control" name="user_email" />
-					</div>
-					<div class="form-group d-flex flex-row flex-wrap justify-content-center">
-						<label class="my-auto" for="message">Message</label>
-						<textarea class="form-control" id="message" rows="4"></textarea>
-					</div>
-					<div class="form-group d-flex flex-row flex-wrap justify-content-center">
-						<button class="btn btn-primary" type="submit">Envoyer</button>
+			<div class="card mx-auto">
+				<!-- Formulaire -->
+				<div class="card-block">
+					<h4 class="card-title text-center">Par email</h4>
+					<form class="" action="" method="post">
+						<div class="form-group row">
+							<label class="col-3 col-form-label" for="prenom">Prénom</label>
+							<div class="col-9">
+								<input type="text" id="prenom"  class="form-control" name="user_firstname"/>
+							</div>
+						</div>						
+						<div class="form-group row">
+							<label class="col-3 col-form-label" for="nom">Nom</label>
+							<div class="col-9">
+								<input type="text" id="nom" class="form-control" name="user_name"/>
+							</div>
+						</div>									
+						<div class="form-group row">
+							<label class="col-3 col-form-label" for="couriel">Email</label>
+							<div class="col-9">
+								<input type="email" id="courriel" class="form-control" name="user_email"/>
+							</div>
 						</div>
-				</form>
+						<div class="form-group row">
+							<label class="col-3 col-form-label" for="message">Message</label>
+							<div class="col-9">
+								<textarea class="form-control" id="message" rows="4"></textarea>
+							</div>
+						</div>
+						<div class="form-group row">
+							<button class="btn btn-primary mx-auto" type="submit">Envoyer</button>
+						</div>
+					</form>
+				</div>
 			</div>
-		</div>
-
-		<div class="card-block text-center">
-			<h4 class="card-title">Plan</h4>
-			<p class="card-text">
-				Opéra Bastille : Place de la Bastille, 75012 Paris<br />
-				Palais Garnier : 8 Rue Scribe, 75009 Paris
-			</p>
-			<!-- div utilisée pour positionner la carte créée par la fonction JavaScript initMap -->
-			<div class="mx-auto" style="width:40rem;height:30rem;" id="map"></div>
-		</div>
+			<div class="card mx-auto">
+				<div class="card-block text-center">
+					<h4 class="card-title">Plan</h4>
+					<p class="card-text">
+						Opéra Bastille : Place de la Bastille, 75012 Paris<br />
+						Palais Garnier : 8 Rue Scribe, 75009 Paris
+					</p>
+					<!-- div utilisée pour positionner la carte créée par la fonction JavaScript initMap -->
+					<div class="mx-auto" style="width:30rem;height:25rem;" id="map"></div>
+				</div>
+			</div>
 	</div>
-</div>
 
-<?php include($path_structure."footer.php"); ?>	<!-- Inclusion pied de page -->
-<?php include($path_structure."JS.php"); ?>	<!-- Inclusion CDN et fichiers javascript -->
+	<?php include($path_structure."footer.php"); ?>	<!-- Inclusion pied de page -->
+	<?php include($path_structure."JS.php"); ?>	<!-- Inclusion CDN et fichiers javascript -->
 </body>
 </html>
