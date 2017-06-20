531ed4fd5cffc4cfdd67695181994ccb5ce9deb5
Bakaniko <baka.niko@gmail.com>
2017-06-20 15:26:23 +0200

    sauvegarde requêtes

--- notes/requetesTypes.php
+++ notes/requetesTypes.php
@@ -63,4 +63,37 @@ $sql = "INSERT INTO
     . "JOIN proj_PrixPlace as prix ON (prix.idCategorie = cat.idCategorie)\n"
     . "where r.date > 20170601 and sa.nom=\"Opéra Bastille\" and prix.Prix < 120";
 
+
+    // récupérer les 6 prochains spectacles
+    <?php
+      //date_format(r.date, "%d %M %Y") as date
+      $sql  = $sql  = 'SELECT spe.nom, r.date as date, spe.type, spe.infos, spe.nomImage, spe.idSpectacle as id
+              FROM  proj_Representation as r
+              JOIN proj_Spectacle as spe ON r.idSpectacle = spe.idSpectacle
+              WHERE r.date >= CURRENT_DATE ORDER BY date ASC LIMIT 6';
+
+      $req = $pdo->query($sql);
+          $compteur=0;
+
+          while ($data = $req->fetch()){
+            if($compteur==0){
+              ?>
+            <h4 class="card-title"><a href="<?php echo $path_pages.'spectacle.php?spectacle='.$data->nom.'&idSpectacle='.$data->id;?>" class="card-link"><?php echo $data->nom;?></a></h4>
+            <h5 class="card-title"><?php echo affDate($data->date);?></h5>
+
+            </div>
+            <img class="card-img-bottom img-fluid d-block mx-auto" src="<?php echo $path_images.$data->nomImage;?>" alt="<?php echo "Affiche de ".$data->nom;?>">
+
+            <p class="card-text"><?php echo $data->infos;?></p>
+
+            <ul class="list-group list-group-flush mt-3">
+            <?php
+            }
+            else{
+              echo '<li class="list-group-item"><a href="'.$path_pages.'spectacle.php?spectacle='.$data->nom.'&idSpectacle='.$data->id.'"class="card-link">'.affDate($data->date)." : ".$data->nom.','.$data->type.'</a></li>';
+            }
+            $compteur++;
+          }
+        $req->closeCursor();
+          ?>
  ?>
