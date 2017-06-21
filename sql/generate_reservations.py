005ca80463221285222c29ccabb69f54b61199cb
Bakaniko <baka.niko@gmail.com>
2017-06-21 19:55:57 +0200

    specifications

--- sql/generate_reservations.py
+++ sql/generate_reservations.py
@@ -12,4 +12,22 @@
 # website url:                              #
 # http://handiman.univ-paris8.fr/~nicolas/  #
 #                                           #
+# inspiration:                              #
+# http://sametmax.com/lencoding-en-python-une-bonne-fois-pour-toute/ #
 #############################################
+
+'''
+Paramètres en entrée:
+- nombre de spectacles en base
+- nombre de clients en base
+- nombre de place dans la salle
+
+format de sortie attendu:
+- 1 ligne = 1 enregistrement
+idUtilisateur;idRepresentation;idPlace
+
+
+encodage de la sortie :
+une_chaine = une_chaine.encode('utf8')
+
+'''
