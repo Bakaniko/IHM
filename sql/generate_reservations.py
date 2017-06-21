33dff1b70f1518c0fae8f73a253d7a210df63c18
Bakaniko <baka.niko@gmail.com>
2017-06-21 19:38:44 +0200

    Premières lignes du script de réservations

---
+++ sql/generate_reservations.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: <utf-8> -*-
+
+#############################################
+# This script generates random reservations #
+# from a few parameters. It delivers it in  #
+# a csv file                                #
+#                                           #
+# project url:                              #
+# https://github.com/MinMinLight/IHM        #
+#                                           #
+# website url:                              #
+# http://handiman.univ-paris8.fr/~nicolas/  #
+#                                           #
+#############################################
