09c6e65646ba8144c49fc5b1c88de6894af8b893
Bakaniko <baka.niko@gmail.com>
2017-06-24 19:32:47 +0200

    Intégration de register.php

--- pages/register.php
+++ pages/register.php
@@ -5,6 +5,9 @@ $path_structure=$path_root."structure/";
 $path_pages=$path_root."pages/";
 $path_images=$path_root."images/";
 ?>
+<?php
+require_once"$path_structure".'captcha.php';
+?>
 <!-- PHP qui gère le formulaire -->
 <?php
 if (!empty($_POST)) {
@@ -12,16 +15,17 @@ if (!empty($_POST)) {
 	require_once"$path_structure".'base.php';# inclure la connection à la base de données pour vérifier si les infos éxistent ou pas
 	require_once"$path_structure".'fonctions.php';# inclure la fonction debug  
 	if (empty($_POST['login'])|| !preg_match('/^[A-Za-z0-9]+$/',$_POST['login'])){
-		$errors['login'] = "Votre login n'est pas valide !";
-	}else{
+    $errors['login'] = "Votre pseudonyme n'est pas valide !";
+  }else{
     $req=$pdo->prepare('SELECT idUtilisateur FROM proj_utilisateur WHERE login =?'); # verifier si le login existe en selectionnant son id
     $req->execute ([$_POST['login']]);
     $iduser=$req->fetch();
     if ($iduser) {
-      $errors['login']='Ce Pseudo est déjà pris !';
-    }	
+      $errors['login']='Ce pseudonyme est existe !';	
   }
-	if (empty($_POST['email'])|| !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)){
+  }if (empty($_POST['reponse'])|| !preg_match('/^[0-9]+$/',$_POST['reponse'])||  $_SESSION['captcha']!=$_POST['reponse']){
+    $errors['reponse'] = "Le résultat du calcul n'est pas valide !";
+  }if (empty($_POST['email'])|| !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)){
 		$errors['email'] = "Votre email n'est pas valide!";
 	}else{
     $req=$pdo->prepare('SELECT idUtilisateur FROM proj_utilisateur WHERE adresseMail =?'); # verifier si le login existe en selectionnant son id
@@ -36,15 +40,20 @@ if (!empty($_POST)) {
 	}
 	if (empty($errors)) {
 		$req=$pdo->prepare("INSERT INTO proj_utilisateur SET login = ?, adresseMail = ?, passHash = ?, code_confirmation = ?"); # requette préparée pour entrer les infos dans le base de données
-		$salt= uniqid(mt_rand(), true);
-		$options=['salt'=>$salt, 'cost'=>12];
+		
+		
     $code_confirme=generer_code(60);
-		$passehash=crypt($_POST['passe'],'$2y$12$'.$salt.'$');
+		$passehash=password_hash($_POST["passe"], PASSWORD_BCRYPT);
 		$req->execute([$_POST['login'],$_POST['email'],$passehash,$code_confirme]);
     $dernierId=$pdo->lastInsertId(); # Récuperer le dernier Id généré par PDO
-    mail($_POST['email'], 'Confirmation de votre compte', "Projet M1MIASH : \n\nAfin de valider votre compte, veuillez ciliquer sur le lien suivant\n\n http://localhost/IHM-master/pages/confirmation.php?id=$dernierId&code_confirme=$code_confirme");# on envois par mail un lien de confirmation qui contient le code et qui est dirigé vers la page de confirmation
-    $_SESSION['flash']['success']='Un email de confirmation vous été envoyé';
-    header('location:inscription_message.php');
+        $message="Votre inscription a bien été prise en compte,\n\n Afin de valider votre compte, veuillez ciliquer sur le lien suivant\n\n http://localhost/IHM/pages/confirmation.php?id=$dernierId&code_confirme=$code_confirme";
+        $header="MIME-Version:1.0\r\n";
+        $header.="From:'nassimyousfi1987@gmail.com'\r\n";
+        $header.='Content-Type:text/html;charset="utf-8"'."\n";
+        $header.='Content-transfer-Encoding:8bit'; 
+    mail($_POST['email'], 'Confirmation de votre inscription à Opéra de Paris',$message, $header);# on envois par mail un lien de confirmation qui contient le code et qui est dirigé vers la page de confirmation
+    $_SESSION['flash']['success']='Inscription réussie';
+    header('Location:inscription_message.php');
     exit();
 	}
 }
@@ -56,7 +65,14 @@ if (!empty($_POST)) {
   <?php include($path_structure."menu.php"); ?> <!-- Inclusion menu -->
 
   <!-- Contenu de la page de connexion -->
-
+ <?php if (isset($_SESSION['flash'])):?>
+  <?php foreach ($_SESSION['flash'] as $type=>$message):?>
+    <div class="alert alert-<?=$type;?>">
+      <?=$message;?>
+    </div>
+  <?php endforeach;?>
+  <?php unset($_SESSION['flash']);?>
+<?php endif;?>
   <div class="container" id="main">
     <div class="card m-auto">
       <!-- Formulaire de connexion -->
@@ -90,6 +106,12 @@ if (!empty($_POST)) {
             <label for="passe_confirm">Confirmation du mot de passe *</label>
             <input type="password"name="passe_confirm" class="form-control mx-auto text-center" id="inputMotDePasse">
           </div>
+            <div class="form-group">
+               <h3> Pour des raisons de sécurité, merci de résoudre l'opération suivante :</h3>
+               <label for="captcha">Combien font <?php echo captcha(); ?></label>
+               <input type="text" name="reponse" class="form-control mx-auto text-center" id="reponse" placeholder="Numérique">
+            </div>
+
           <div class="form-check">
             <label class="form-check-label">
               <input type="checkbox" class="form-check-input">
