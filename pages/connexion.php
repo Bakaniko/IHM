34c0df52e46020d0367def77fd54ec4644a61a0d
MinMinLight <cascio.email@gmail.com>
2017-06-24 16:52:36 +0200

    mise a jour inscription.php connexion.php

--- pages/connexion.php
+++ pages/connexion.php
@@ -17,39 +17,39 @@ $path_pages=$path_root."pages/";
 $path_images=$path_root."images/";
 ?>
 <!DOCTYPE html>
-<html lang="fr" class="">
+<html lang="fr">
 <?php include($path_structure."head.php"); ?> <!-- Inclusion <head> -->
-<body class="">
+<body>
 	<?php include($path_structure."menu.php"); ?> <!-- Inclusion menu -->
 
 	<!-- Contenu de la page de connexion -->
 
-  <div class="container" id="main">
-      <!-- Formulaire de connexion -->
-      <div class="card m-auto" id="cardConnexion">
-          <div class="card-block text-center">
-              <h4 class="card-title">Formulaire de connexion</h4>
-          </div>
-          <form class="text-center" method="POST" action="">
-              <!-- Informations de connexion -->
-              <div class="form-group">
-                  <label for="Login">Login</label>
-                  <input type="text class" class="form-control mx-auto text-center" name="Login" placeholder="LapinAnonyme">
-              </div>
-              <div class="form-group">
-                  <label for="MotDePasse">Mot de passe</label>
-                  <input type="password" class="form-control mx-auto text-center" name="MotDePasse" placeholder="************">
-              </div>
-              <button type="submit" class="btn btn-primary" value="Envoyer">Connexion</button>
-          </form>
-          <!-- Boutton d'envoi -->
-          <div class="card-block text-center">
-              <p class="card-text">Vous n'êtes pas encore inscrit ?</p>
-              <a class="btn btn-secondary" href="<?php echo $path_pages ; ?>inscription.php" role="button">Inscription</a>
-          </div>
-          <!-- Fin du forulaire de connexion -->
-      </div>
-  </div>
+    <div class="container" id="main">
+        <!-- Formulaire de connexion -->
+        <div class="card mx-auto" id="cardConnexion">
+            <div class="card-block text-center">
+                <h4 class="card-title">Formulaire de connexion</h4>
+            </div>
+            <form class="text-center" method="POST" action="">
+                <!-- Informations de connexion -->
+                <div class="form-group">
+                    <label for="Login">Login</label>
+                    <input type="text class" class="form-control mx-auto text-center" name="Login" placeholder="LapinAnonyme">
+                </div>
+                <div class="form-group">
+                    <label for="MotDePasse">Mot de passe</label>
+                    <input type="password" class="form-control mx-auto text-center" name="MotDePasse" placeholder="************">
+                </div>
+                <button type="submit" class="btn btn-primary" value="Envoyer">Connexion</button>
+            </form>
+            <!-- Boutton d'envoi -->
+            <div class="card-block text-center">
+                <p class="card-text">Vous n'êtes pas encore inscrit ?</p>
+                <a class="btn btn-secondary" href="<?php echo $path_pages ; ?>inscription.php" role="button">Inscription</a>
+            </div>
+            <!-- Fin du forulaire de connexion -->
+        </div>
+    </div>
 
   <?php include($path_structure."footer.php"); ?> <!-- Inclusion pied de page -->
   <?php include($path_structure."JS.php"); ?>  <!-- Inclusion CDN et fichiers javascript -->
