713cee9e2e248b70aa2bfbea87c2fc7493b9272a
Bakaniko <baka.niko@gmail.com>
2017-06-25 13:56:19 +0200

    WIP sur compte.php

--- pages/compte.php
+++ pages/compte.php
@@ -1,15 +1,27 @@
-<?php if (session_status()==PHP_SESSION_NONE) {session_start();}?> 
-<?php if (!isset($_SESSION['auth'])){
-$_SESSION['flash']['danger']="Vous n'avez pas le droit d'accèder à cette page";
-header('Location:connexion.php');
-}?>
-
-
-<?php // Définition des chemins d'accès aux fichiers
+<?php if (session_status()==PHP_SESSION_NONE) {session_start();}?>
+<?php
+// Définition des chemins d'accès aux fichiers
 $path_root="../";
 $path_structure=$path_root."structure/";
 $path_pages=$path_root."pages/";
 $path_images=$path_root."images/";
+
+require_once"$path_structure".'fonctions.php';# inclure la fonction debug  <?php session_start();
+if (!isset($_SESSION['auth'])){
+$_SESSION['flash']['danger']="Vous n'avez pas le droit d'accèder à cette page";
+header('Location:connexion.php');
+}
+else {
+	debug($_SESSION);
+}
+
+?>
+
+
+<?php
+// Accès à la base
+require_once"$path_structure".'base.php';# inclure la connection à la base de données pour vérifier si les infos éxistent ou pas
+
 ?>
 <!-- PHP qui gère le formulaire -->
 <?php
@@ -99,8 +111,7 @@ if (!empty($_POST)) {
 
 <?php
 
-	require_once"$path_structure".'base.php';# inclure la connection à la base de données pour vérifier si les infos éxistent ou pas
-	require_once"$path_structure".'fonctions.php';# inclure la fonction debug  <?php session_start();
+
 ?>
 
 <!DOCTYPE html>
@@ -128,61 +139,86 @@ if (!empty($_POST)) {
 		</div>
       <!-- Formulaire de modification -->
       <div class="card-block text-center">
-        <h4 class="card-title">Modifiez les informations de votre profil</h4>
+        <h4 class="card-title">Modifiez vos identifiants</h4>
       </div>
       <form action="" method="POST">
-        <div class="form-group row">
-          <label for="inputpseudo" class="col-3 col-form-label">Modifier votre pseudo</label>
-          <div class="col-9">
-            <input type="text" class="form-control" name="inputpseudo" id="inputpseudo">
-          </div>
-        </div>
+					<div class="form-group row">
+						<label for="inputpseudo" class="col-3 col-form-label">Modifier votre pseudo</label>
+						<div class="col-9">
+							<input type="text" class="form-control" name="inputpseudo" id="inputpseudo" value=<?php echo $_SESSION['auth']->login; ?>>
+						</div>
+					</div>
+		<div class="form-group row">
+			<label for="inputMotDePasse" class="col-3 col-form-label"> Modifier votre mot de passe</label>
+			<div class="col-9">
+				<input type="password" class="form-control" name="inputMotDePasse" id="inputMotDePasse">
+			</div>
+		</div>
+		 <div class="form-group row">
+			<label for="inputMotDePasse2" class="col-3 col-form-label"> Confirmer le nouveau mot de passe</label>
+			<div class="col-9">
+				<input type="password" class="form-control" name="inputMotDePasse2" id="inputMotDePasse2">
+			</div>
+		</div>
+		<div class="form-group row">
+			<button type="submit" class="btn btn-primary mx-auto">Enregistrer vos modifications</button>
+		</div>
+	</form>
+
+		<div class="card mx-auto text-center">
+			<h2>Mes coordonnées</h2>
+		</div>
+      <!-- Formulaire de modification -->
+      <div class="card-block text-center">
+        <h4 class="card-title">Modifiez vos coordonnées</h4>
+      </div>
+      <form action="" method="POST">
          <div class="form-group row">
           <label for="inputNom" class="col-3 col-form-label">Nom</label>
           <div class="col-9">
-            <input type="text" class="form-control" id="inputNom" name="inputNom">
+            <input type="text" class="form-control" id="inputNom" name="inputNom" value=<?php echo $_SESSION['auth']->nom; ?>>
           </div>
         </div>
         <div class="form-group row">
           <label for="inputPrenom" class="col-3 col-form-label">Prénom</label>
           <div class="col-9">
-            <input type="text" class="form-control" id="inputPrenom" name="inputPrenom">
+            <input type="text" class="form-control" id="inputPrenom" name="inputPrenom" value=<?php echo $_SESSION['auth']->prenom; ?>>
           </div>
         </div>
         <div class="form-group row">
           <label for="inputEmail" class="col-3 col-form-label">Adresse email</label>
           <div class="col-9">
-            <input type="email" class="form-control" name="inputEmail" id="inputEmail" aria-describedby="emailHelp">
+            <input type="email" class="form-control" name="inputEmail" id="inputEmail" aria-describedby="emailHelp" value=<?php echo $_SESSION['auth']->adresseMail; ?>>
           </div>
         </div>
         <div class="form-group row">
           <label for="inputAdressePostale1" class="col-3 col-form-label">Adresse postale 1</label>
           <div class="col-9">
-            <input type="text" class="form-control" id="inputAdressePostale1" name="inputAdressePostale1">
+            <input type="text" class="form-control" id="inputAdressePostale1" name="inputAdressePostale1" value=<?php echo $_SESSION['auth']->adressePostale1; ?>>
           </div>
         </div>
         <div class="form-group row">
           <label for="inputAdressePostale2" class="col-3 col-form-label">Adresse postale 2</label>
           <div class="col-9">
-          <input type="text" class="form-control" id="inputAdressePostale2"  name="inputAdressePostale2">
+          <input type="text" class="form-control" id="inputAdressePostale2"  name="inputAdressePostale2" value=<?php echo $_SESSION['auth']->adressePostale2; ?>>
           </div>
         </div>
         <div class="form-group row">
           <label for="inputCodePostal" class="col-3 col-form-label">Code postal</label>
           <div class="col-9">
-            <input type="text" class="form-control" id="inputCodePostal" name="inputCodePostal" placeholder="88600">
+            <input type="text" class="form-control" id="inputCodePostal" name="inputCodePostal" value=<?php echo $_SESSION['auth']->codePostal; ?>>
           </div>
         </div>
         <div class="form-group row">
-          <label for="inputVille" class="col-3 col-form-label" class="col-2 col-form-label">Ville</label>
+          <label for="inputVille" class="col-3 col-form-label">Ville</label>
           <div class="col-9">
-            <input type="text" class="form-control" id="inputVille" name="inputVille">
+            <input type="text" class="form-control" id="inputVille" name="inputVille" value=<?php echo $_SESSION['auth']->ville; ?>>
           </div>
         </div>
         <div class="form-group row">
           <label for="inputTel" class="col-3 col-form-label">Téléphone</label>
           <div class="col-9">
-            <input class="form-control" type="tel" value="1-(555)-555-5555" name="inputTel" id="inputTel">
+            <input class="form-control" type="tel" value="1-(555)-555-5555" name="inputTel" id="inputTel" value=<?php echo $_SESSION['auth']->telephone; ?>>
           </div>
         </div>
         <div class="form-group row">
@@ -203,8 +239,6 @@ if (!empty($_POST)) {
       </form>
     </div>
     <!-- Fin du formulaire -->
-  </div>
-</div>
 <?php include($path_structure."footer.php"); ?> <!-- Inclusion pied de page -->
 <?php include($path_structure."JS.php"); ?>  <!-- Inclusion CDN et fichiers javascript -->
 </body>
